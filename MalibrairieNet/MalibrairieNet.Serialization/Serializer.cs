82944fd8f10ecc6a43eff64a5f6972b9b61b1d14
sandra nguessong <sandranguessong@gmail.com>
2021-10-16 21:12:57 +0100

    Add serialize and deserialize method

--- MalibrairieNet/MalibrairieNet.Serialization/Serializer.cs
+++ MalibrairieNet/MalibrairieNet.Serialization/Serializer.cs
@@ -1,8 +1,29 @@
 ﻿using System;
-
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 namespace MalibrairieNet.Serialization
 {
     public class Serializer
     {
+        public void Serialize<T>(T data, string path)
+        {
+            using (StreamWriter file = new StreamWriter(path, true))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(file.BaseStream, data);
+            }
+
+        }
+        public T Deserialize<T>(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                return (T)bf.Deserialize(file.BaseStream);
+            }
+        }
     }
 }
+     
+     
+
